e879bad4164b817e4a28a1583389ff683dbabfbb
Ricardo Perez <ricardo.pereze.98@gmail.com>
2022-09-03 10:16:33 -0400

    [ADD] eslint plugins

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    customKey: 'customValue'
+  },
+  basePath: '/dist',
+  compress: true,
+  async redirects(){
+    return[
+      {
+        sourse: '/hola',
+        destunation: 'ricardodev.com',
+        permanent: true
+      }]
+  },
   swcMinify: true,
 }
 
